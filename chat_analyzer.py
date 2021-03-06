df2823191996586811263c34e0f82171627304f1
Ravi <jravi248@gmail.com>
2021-03-06 14:41:02 +0530

    Read from file only once

--- chat_analyzer.py
+++ chat_analyzer.py
@@ -25,96 +25,99 @@ DateTime = Date + Time # To get the date and time combined (Don't know why I add
 
 Msg = Date + Time + User + '(?P<message>.*)' # Finally to get the parsed message
 
+def import_data(path_to_chatfile):
+    file = open(path_to_chatfile, "r")
+    msgs = []
+    for line in file:
+        match = re.search(Msg, line)
+        if match:
+            msgs.append({
+                            'username': match.groupdict()['username'],
+                            'date': match.groupdict()['date'],
+                            'month': match.groupdict()['month'],
+                            'day': match.groupdict()['day'],
+                            'year': match.groupdict()['year'],
+                            'time': match.groupdict()['time'],
+                            'hour': match.groupdict()['hour'],
+                            'minute': match.groupdict()['minute'],
+                        })
+    file.close()
+    return msgs
 
-def find_msg_count(chatfile, start_date=None, end_date=None):
+def find_msg_count(msgs, start_date=None, end_date=None):
     '''
     Find the number of messages
     '''
-    file = open(chatfile, "r")
     count = 0
-    for line in file:
-        match = re.search(Msg, line)
-        if match:
-            matched_date = datetime.strptime(match.groupdict()['date'], '%m/%d/%y').date()
-            if not start_date or (start_date and start_date < matched_date < end_date):
-                count += 1
-    file.close()
+    for msg in msgs:
+        date = datetime.strptime(msg['date'], '%m/%d/%y').date()
+        if not start_date or (start_date and start_date < date < end_date):
+            count += 1
     return count
 
 
-def find_freq(chatfile, username=None, start_date=None, end_date=None):
+def find_freq(msgs, username=None, start_date=None, end_date=None):
     '''
     Find the frequecy at which a given users messages
     '''
-    file = open(chatfile, 'r')
     user_count = {}
-    for line in file:
-        match = re.search(Msg, line)
-        if match:
-            matched_user = match.groupdict()['username']
-            matched_date = datetime.strptime(match.groupdict()['date'], '%m/%d/%y').date()
-            if not start_date or (start_date and start_date < matched_date < end_date):
-                user_count[match.groupdict()['username']] = user_count[match.groupdict()['username']] + 1 if match.groupdict()['username'] in user_count else 1
-    file.close()
+    for msg in msgs:
+        user = msg['username']
+        date = datetime.strptime(msg['date'], '%m/%d/%y').date()
+        if not start_date or (start_date and start_date < date < end_date):
+            user_count[user] = user_count[user] + 1 if user in user_count else 1
     if username:
         return user_count[username] if username in user_count else 0
     else:
         return user_count
 
 
-def calc_percentage(path_to_chatfile, username=None, start_date=None, end_date=None):
+def calc_percentage(msgs, username=None, start_date=None, end_date=None):
     '''
     Calc the metrics of how much the entered user has spoken in the chat within the given constraint(if provided)
     '''
-    user_count = find_freq(path_to_chatfile, username, start_date, end_date)
-    total_count = find_msg_count(path_to_chatfile, start_date, end_date)
+    user_count = find_freq(msgs, username, start_date, end_date)
+    total_count = find_msg_count(msgs, start_date, end_date)
 
     print('Total Count: {}\n'.format(total_count))
 
     if username:
-        print('User Count: {}'.format(user_count))
+        print('Message Count: {}'.format(user_count))
         print('Percentage: {}'.format(user_count/total_count*100))
     else:
         for user, count in user_count.items():
             print("For the user {}".format(user))
-            print("User Count: {}".format(count))
+            print("Message Count: {}".format(count))
             print("Percentage: {}\n".format(count/total_count*100))
 
 
-def find_conv_starters(path_to_chatfile, username=None):
+def find_conv_starters(msgs, username=None):
     '''
     Find out who has started a conversations how many times
     '''
-    file = open(path_to_chatfile, 'r')
     total_diff = 0
     count = 1
     last_msg = None
 
     # Get the first message that was sent in the chat
-    m = None
-    while not m:
-        m = re.match(Msg, file.readline())
-    last_msg = datetime.strptime(m.groupdict()['date'] + ' ' + m.groupdict()['time'], '%m/%d/%y %H:%M')
+    last_msg = datetime.strptime(msgs[0]['date'] + ' ' + msgs[0]['time'], '%m/%d/%y %H:%M')
     user_count = {}
-    for line in file:
-        m = re.match(Msg, line)
-        if m:
-            curr_msg = datetime.strptime(m.groupdict()['date'] + ' ' + m.groupdict()['time'], '%m/%d/%y %H:%M')
-            difference = (curr_msg - last_msg).total_seconds()
-            if difference != 0:
-                total_diff += difference
-                average = total_diff / count
-                '''
-                If the difference in time between current message and the last message is more than the average difference
-                then that would mean that the current message is a conversation starter
-                '''
-                if difference > average:
-                    user_count[m.groupdict()['username']] = user_count[m.groupdict()['username']] + 1 if m.groupdict()['username'] in user_count else 1
-                    total_diff = 0
-                    count = 0
-                last_msg = curr_msg
-                count += 1
-    file.close()
+    for msg in msgs:
+        curr_msg = datetime.strptime(msg['date'] + ' ' + msg['time'], '%m/%d/%y %H:%M')
+        difference = (curr_msg - last_msg).total_seconds()
+        if difference != 0:
+            total_diff += difference
+            average = total_diff / count
+            '''
+            If the difference in time between current message and the last message is more than the average difference
+            then that would mean that the current message is a conversation starter
+            '''
+            if difference > average:
+                user_count[msg['username']] = user_count[msg['username']] + 1 if msg['username'] in user_count else 1
+                total_diff = 0
+                count = 0
+            last_msg = curr_msg
+            count += 1
 
     if username:
         print("The user {} started consversation {} time(s)".format(username, user_count[username]))
@@ -123,11 +126,10 @@ def find_conv_starters(path_to_chatfile, username=None):
             print("The user {} started consversation {} time(s)".format(user, count))
 
 
-def check_activity(path_to_chatfile, username=None, start_date=None, end_date=None):
+def check_activity(msgs, username=None, start_date=None, end_date=None):
     '''
     Get the time of the day when each user(or a particular user) is most active
     '''
-    file = open(path_to_chatfile, 'r')
     '''
     Prototype for the user_count variable
     user_count = {
@@ -137,16 +139,14 @@ def check_activity(path_to_chatfile, username=None, start_date=None, end_date=No
     }
     '''
     user_count = {}
-    for line in file:
-        match = re.search(Msg, line)
-        if match:
-            matched_user = match.groupdict()['username']
-            matched_hour = match.groupdict()['hour']
-            matched_date = datetime.strptime(match.groupdict()['date'], '%m/%d/%y').date()
-            if not start_date or (start_date and start_date < matched_date < end_date):
-                if matched_user not in user_count:
-                    user_count[matched_user] = {}
-                user_count[matched_user][matched_hour] = user_count[matched_user][matched_hour] + 1 if matched_hour in user_count[matched_user] else 1
+    for msg in msgs:
+        user = msg['username']
+        hour = msg['hour']
+        date = datetime.strptime(msg['date'], '%m/%d/%y').date()
+        if not start_date or (start_date and start_date < date < end_date):
+            if user not in user_count:
+                user_count[user] = {}
+            user_count[user][hour] = user_count[user][hour] + 1 if hour in user_count[user] else 1
 
     for user in user_count:
         max_freq = 0
@@ -163,37 +163,33 @@ def check_activity(path_to_chatfile, username=None, start_date=None, end_date=No
     else:
         for user in user_count:
             print("The user {} mostly stays active around {} Hours".format(user, user_count[user]['max']))
-    file.close()
 
 
-def interaction_curve_func(chatfile, username=None, start_date=None, end_date=None, show_graph=False):
+def interaction_curve_func(msgs, username=None, start_date=None, end_date=None, show_graph=False):
     '''
     Make a linear regression model to predict whether there has been
     an increase or decrease in the number of messages
     '''
-    file = open(chatfile, "r")
     cur_date = ""
     cur_freq = 0
     dates = []
     str_dates = []
     freqs = []
 
-    for line in file:
-        match = re.search(Msg, line)
-        if match:
-            matched_date = datetime.strptime(match.groupdict()['date'], '%m/%d/%y').date()
-            matched_user = match.groupdict()['username']
+    for msg in msgs:
+        date = datetime.strptime(msg['date'], '%m/%d/%y').date()
+        user = msg['username']
 
-            if (not username or matched_user == username) and (not start_date or (matched_date >= start_date and matched_date <= end_date)):
-                if cur_date == "":
-                    cur_date = matched_date
-                elif matched_date != cur_date:
-                    dates.append(datetime.toordinal(cur_date))
-                    str_dates.append(str(cur_date))
-                    freqs.append(cur_freq)
-                    cur_date = matched_date
-                    cur_freq = 0
-                cur_freq += 1
+        if (not username or user == username) and (not start_date or (date >= start_date and date <= end_date)):
+            if cur_date == "":
+                cur_date = date
+            elif date != cur_date:
+                dates.append(datetime.toordinal(cur_date))
+                str_dates.append(str(cur_date))
+                freqs.append(cur_freq)
+                cur_date = date
+                cur_freq = 0
+            cur_freq += 1
 
     dates.append(datetime.toordinal(cur_date))
     str_dates.append(str(cur_date))
@@ -221,8 +217,6 @@ def interaction_curve_func(chatfile, username=None, start_date=None, end_date=No
         plt.locator_params(axis='x', nbins=4)
         plt.show()
 
-    file.close()
-
 
 '''
 The command line options
@@ -237,6 +231,7 @@ The command line options
 @click.option('-iC', '--interaction-curve', is_flag=True, help='Tell whether the interaction of the user has increased or decreased')
 @click.option('-sG', '--show-graph', is_flag=True, help='Show graph(s) for the selected options if available')
 def controller(path_to_chatfile, username, percentage, constraint, conv_starters, activity, interaction_curve, show_graph):
+    msgs = import_data(path_to_chatfile)
     start = time()
     if constraint:
         start_date = datetime.strptime(constraint[0], '%m/%d/%y').date()
@@ -244,14 +239,14 @@ def controller(path_to_chatfile, username, percentage, constraint, conv_starters
     else:
         start_date = None
         end_date = None
-    if interaction_curve:
-        interaction_curve_func(path_to_chatfile, username=username, start_date=start_date, end_date=end_date, show_graph=show_graph)
     if conv_starters:
-        find_conv_starters(path_to_chatfile, username)
+        find_conv_starters(msgs, username)
     if percentage:
-        calc_percentage(path_to_chatfile, username, start_date, end_date)
+        calc_percentage(msgs, username, start_date, end_date)
     if activity:
-        check_activity(path_to_chatfile, username, start_date, end_date)
+        check_activity(msgs, username, start_date, end_date)
+    if interaction_curve:
+        interaction_curve_func(msgs, username=username, start_date=start_date, end_date=end_date, show_graph=show_graph)
     end = time()
     print("Program Finished")
     print("Total time taken: {} seconds".format(end - start))
