a17cb4ce30dde79d733c5f705cd1b1a13a2840b4
Ravi <jravi248@gmail.com>
2021-05-03 21:26:52 +0530

    Correct regex

--- src/chat_analyzer.py
+++ src/chat_analyzer.py
@@ -14,7 +14,7 @@ SDate = '(?P<date>(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2}))'
 STime = '(?P<time>(?P<hour>[0-9]{2}):(?P<minute>[0-9]{2}))'
 SDateTime = '\[' + SDate + ' ' + STime + '\]'
 SUser = '(?P<username>[^:]*):'
-SMsg = SDateTime + SUser + '(?P<message>.*)'
+SMsg = SDateTime + ' ' + SUser + '(?P<message>.*)'
 
 # For Telegram chat exports
 TDate = '(?P<date>(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2}))'
