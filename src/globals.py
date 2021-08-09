eb732102e9dfd44b1356273cf3a99670c9ddc824
Ravi Jain <jravi248@gmail.com>
2021-08-10 02:50:49 +0530

    Add ability to recognize self exported data, and capitalize hours_list in globals

--- src/globals.py
+++ src/globals.py
@@ -13,5 +13,5 @@ def init() -> None:
         print('Warning: Tkinter is not installed, graphs will not be shown')
         CAN_SHOW_GRAPH = False
 
-    hours_list = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+    HOURS_LIST = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
                   '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
