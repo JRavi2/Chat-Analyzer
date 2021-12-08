9fedd3913dc2c797a4b7dd8261a47424b5d02fce
sasi <sasi>
2021-12-08 11:50:35 +0530

    Date and time issue in whatsapp chat

--- src/chat_analyzer.py
+++ src/chat_analyzer.py
@@ -20,12 +20,28 @@ Define Regex Patterns
 """
 # For Whatsapp chat exports
 WUser = r'(- (?P<username>[^:]*):)'  # To get the user's name
-# To get the date
-WDate = r'(?P<date>(?P<month>[0-9]{1,2})[-\/]{1}(?P<day>[0-9]{1,2})[-\/]{1}(?P<year>[0-9]{2}))'
-# To get the time
-WTime = r'(, (?P<time>(?P<hour>[0-9]{2}):(?P<minute>[0-9]{2})) )'
+
+# To get the date if it is in the format m/d/y(type1)
+WDatetype1 = r'(?P<datetype1>(?P<monthtype1>[0-9]{1,2})[-\/]{1}(?P<daytype1>[0-9]{1,2})[-\/]{1}(?P<yeartype1>[0-9]{2,4}))'
+# To get the date if it is in the format d/m/y(type2)
+WDatetype2 = r'(?P<datetype2>(?P<daytype2>[0-9]{1,2})[-\/]{1}(?P<monthtype2>[0-9]{1,2})[-\/]{1}(?P<yeartype2>[0-9]{2,4}))'
+# To get the date if it is in the format y/m/d(type3)
+WDatetype3 = r'(?P<datetype3>(?P<yeartype3>[0-9]{2,4})[-\/]{1}(?P<monthtype3>[0-9]{1,2})[-\/]{1}(?P<daytype3>[0-9]{1,2}))'
+
+# To get the time in 12hr clock
+WTimeHour12 = r'(, (?P<time>(?P<hour>[0-9]{1,2}):(?P<minute>[0-9]{2}) (?P<ampm>[AP]M)) )'
+# To get the time in 24hr clock
+WTimeHour24 = r'(, (?P<time>(?P<hour>[0-9]{1,2}):(?P<minute>[0-9]{2})) )'
+
 # Finally to get the parsed message
-WMsg = WDate + WTime + WUser + r'(?P<message>.*)'
+WMsgtype1Hour12 = WDatetype1 + WTimeHour12 + WUser + r'(?P<message>.*)'
+WMsgtype1Hour24 = WDatetype1 + WTimeHour24 + WUser + r'(?P<message>.*)'
+
+WMsgtype2Hour12 = WDatetype2 + WTimeHour12 + WUser + r'(?P<message>.*)'
+WMsgtype2Hour24 = WDatetype2 + WTimeHour24 + WUser + r'(?P<message>.*)'
+
+WMsgtype3Hour12 = WDatetype3 + WTimeHour12 + WUser + r'(?P<message>.*)'
+WMsgtype3Hour24 = WDatetype3 + WTimeHour24 + WUser + r'(?P<message>.*)'
 
 # For Signal chat exports
 SDate = r'(?P<date>(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2}))'
@@ -150,8 +166,13 @@ def import_data(path_to_chatfile: str) -> List[Dict[str, Any]]:
     for line_num, line in enumerate(f):
         if line_num > globals.WHATSAPP_MEMBER_LIMIT:
             break
-        match = re.search(WMsg, line)
-        if match:
+        match1Hour12 = re.search(WMsgtype1Hour12, line)
+        match1Hour24 = re.search(WMsgtype1Hour24, line)
+        match2Hour12 = re.search(WMsgtype2Hour12, line)
+        match2Hour24 = re.search(WMsgtype2Hour24, line)
+        match3Hour12 = re.search(WMsgtype3Hour12, line)
+        match3Hour24 = re.search(WMsgtype3Hour24, line)
+        if match1Hour12 or match1Hour24 or match2Hour12 or match2Hour24 or match3Hour12 or match3Hour24 :
             isWa = True
             break
 
@@ -159,20 +180,81 @@ def import_data(path_to_chatfile: str) -> List[Dict[str, Any]]:
         print('Whatsapp chat recognized')
         f.seek(0)
         for line in f:
-            match = re.search(WMsg, line)
-            if match:
+            match1Hour12 = re.search(WMsgtype1Hour12, line)
+            match1Hour24 = re.search(WMsgtype1Hour24, line)
+            match2Hour12 = re.search(WMsgtype2Hour12, line)
+            match2Hour24 = re.search(WMsgtype2Hour24, line)
+            match3Hour12 = re.search(WMsgtype3Hour12, line)
+            match3Hour24 = re.search(WMsgtype3Hour24, line)
+            if match1Hour12 and 1<=int(match1Hour12.groupdict()['daytype1'])<=31 and 1<=int(match1Hour12.groupdict()['monthtype1'])<=12 :
                 msgs.append({
-                    'username': match.groupdict()['username'],
-                    'date': datetime.strptime(match.groupdict()['date'], '%m/%d/%y').date(),
-                    'month': match.groupdict()['month'],
-                    'day': match.groupdict()['day'],
-                    'year': match.groupdict()['year'],
-                    'time': datetime.strptime(match.groupdict()['time'], '%H:%M').time(),
-                    'hour': match.groupdict()['hour'],
-                    'minute': match.groupdict()['minute'],
+                    'username': match1Hour12.groupdict()['username'],
+                    'date': datetime.strptime(match1Hour12.groupdict()['datetype1'], '%m/%d/%y').date(),
+                    'month': match1Hour12.groupdict()['monthtype1'],
+                    'day': match1Hour12.groupdict()['daytype1'],
+                    'year': match1Hour12.groupdict()['yeartype1'],
+                    'time': datetime.strptime(match1Hour12.groupdict()['time'], '%I:%M %p').time(),
+                    'hour': match1Hour12.groupdict()['hour'],
+                    'minute': match1Hour12.groupdict()['minute'],
+                })
+            elif match1Hour24 and 1<=int(match1Hour24.groupdict()['daytype1'])<=31 and 1<=int(match1Hour24.groupdict()['monthtype1'])<=12 :
+                msgs.append({
+                    'username': match1Hour24.groupdict()['username'],
+                    'date': datetime.strptime(match1Hour24.groupdict()['datetype1'], '%m/%d/%y').date(),
+                    'month': match1Hour24.groupdict()['monthtype1'],
+                    'day': match1Hour24.groupdict()['daytype1'],
+                    'year': match1Hour24.groupdict()['yeartype1'],
+                    'time': datetime.strptime(match1Hour24.groupdict()['time'], '%H:%M').time(),
+                    'hour': match1Hour24.groupdict()['hour'],
+                    'minute': match1Hour24.groupdict()['minute'],
+                })
+            elif match2Hour12 and 1<=int(match2Hour12.groupdict()['daytype2'])<=31 and 1<=int(match2Hour12.groupdict()['monthtype2'])<=12 :
+                msgs.append({
+                    'username': match2Hour12.groupdict()['username'],
+                    'date': datetime.strptime(match2Hour12.groupdict()['datetype2'], '%d/%m/%y').date(),
+                    'month': match2Hour12.groupdict()['monthtype2'],
+                    'day': match2Hour12.groupdict()['daytype2'],
+                    'year': match2Hour12.groupdict()['yeartype2'],
+                    'time': datetime.strptime(match2Hour12.groupdict()['time'], '%I:%M %p').time(),
+                    'hour': match2Hour12.groupdict()['hour'],
+                    'minute': match2Hour12.groupdict()['minute'],
+                })
+            elif match2Hour24 and 1<=int(match2Hour24.groupdict()['daytype2'])<=31 and 1<=int(match2Hour24.groupdict()['monthtype2'])<=12 :
+                msgs.append({
+                    'username': match2Hour24.groupdict()['username'],
+                    'date': datetime.strptime(match2Hour24.groupdict()['datetype2'], '%d/%m/%y').date(),
+                    'month': match2Hour24.groupdict()['monthtype2'],
+                    'day': match2Hour24.groupdict()['daytype2'],
+                    'year': match2Hour24.groupdict()['yeartype2'],
+                    'time': datetime.strptime(match2Hour24.groupdict()['time'], '%H:%M').time(),
+                    'hour': match2Hour24.groupdict()['hour'],
+                    'minute': match2Hour24.groupdict()['minute'],
+                })
+            elif match3Hour12 and 1<=int(match3Hour12.groupdict()['daytype3'])<=31 and 1<=int(match3Hour12.groupdict()['monthtype3'])<=12 :
+                msgs.append({
+                    'username': match3Hour12.groupdict()['username'],
+                    'date': datetime.strptime(match3Hour12.groupdict()['datetype3'], '%d/%m/%y').date(),
+                    'month': match3Hour12.groupdict()['monthtype3'],
+                    'day': match3Hour12.groupdict()['daytype3'],
+                    'year': match3Hour12.groupdict()['yeartype3'],
+                    'time': datetime.strptime(match3Hour12.groupdict()['time'], '%I:%M %p').time(),
+                    'hour': match3Hour12.groupdict()['hour'],
+                    'minute': match3Hour12.groupdict()['minute'],
+                })
+            elif match3Hour24 and 1<=int(match3Hour24.groupdict()['daytype3'])<=31 and 1<=int(match3Hour24.groupdict()['monthtype3'])<=12 :
+                msgs.append({
+                    'username': match3Hour24.groupdict()['username'],
+                    'date': datetime.strptime(match3Hour24.groupdict()['datetype3'], '%y/%m/%d').date(),
+                    'month': match3Hour24.groupdict()['monthtype3'],
+                    'day': match3Hour24.groupdict()['daytype3'],
+                    'year': match3Hour24.groupdict()['yeartype3'],
+                    'time': datetime.strptime(match3Hour24.groupdict()['time'], '%H:%M').time(),
+                    'hour': match3Hour24.groupdict()['hour'],
+                    'minute': match3Hour24.groupdict()['minute'],
                 })
         f.close()
         return msgs
+
     f.close()
 
     print('Invalid file!')
