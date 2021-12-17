1be6079eb5cd0f36f61bf4a3a4d3bbe1a1201158
Anit Mangal <anit.mangal@gmail.com>
2021-12-18 01:19:52 +0530

    Minor minor changes to streamline-io
    - Replaced hours_list with HOURS_LIST and removed temporary comments

--- src/globals.py
+++ src/globals.py
@@ -4,7 +4,7 @@ import matplotlib
 def init() -> None:
     """Initialize the global variables"""
     global CAN_SHOW_GRAPH
-    global hours_list
+    global HOURS_LIST
 
     try:
         matplotlib.use('TkAgg')
@@ -12,5 +12,5 @@ def init() -> None:
     except ImportError:
         print('Warning: Tkinter is not installed, graphs will not be shown')
         CAN_SHOW_GRAPH = False
-    hours_list = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+    HOURS_LIST = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
                   '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
